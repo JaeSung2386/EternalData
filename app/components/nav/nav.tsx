9e0ee378640c3eccf56de9802abc1ba4d058c924
강재성 <wotjd2386@douzone.com>
2023-08-17 12:54:15 +0900

    완료: nav 컴포넌트 ui

---
+++ app/components/nav/nav.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import "@/scss/index.scss";
+
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { TiGroupOutline } from "react-icons/ti";
+import { BiMessageRoundedDots } from "react-icons/bi";
+import { BsArrowDownCircle } from "react-icons/bs";
+
+function nav() {
+  const handleIconClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeActive(e.currentTarget);
+  };
+
+  const changeActive = (target) => {};
+
+  return (
+    <div className="navigation">
+      <a href="#home">
+        <AiOutlineHome
+          className="icon active-nav"
+          onClick={(e: React.ChangeEvent<HTMLInputElement>) =>
+            handleIconClick(e)
+          }
+        />
+      </a>
+      <a href="#about">
+        <AiOutlineUser
+          className="icon active-nav"
+          onClick={(e: React.ChangeEvent<HTMLInputElement>) =>
+            handleIconClick(e)
+          }
+        />
+      </a>
+      <a href="#members">
+        <TiGroupOutline
+          className="icon active-nav"
+          onClick={(e: React.ChangeEvent<HTMLInputElement>) =>
+            handleIconClick(e)
+          }
+        />
+      </a>
+      <a href="#contact">
+        <BiMessageRoundedDots
+          className="icon active-nav"
+          onClick={(e: React.ChangeEvent<HTMLInputElement>) =>
+            handleIconClick(e)
+          }
+        />
+      </a>
+      <a href="#footer">
+        <BsArrowDownCircle
+          className="icon active-nav"
+          onClick={(e: React.ChangeEvent<HTMLInputElement>) =>
+            handleIconClick(e)
+          }
+        />
+      </a>
+    </div>
+  );
+}
+
+export default nav;
